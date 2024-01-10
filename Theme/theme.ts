fcc5bd90513fabbd2f61df6416ce20eddb89f87f
Sadaqat Dahani <sadaqat.dahani2013@gmail.com>
2024-01-10 23:55:57 +0500

    Updated theme file and added more colors.

--- Theme/theme.ts
+++ Theme/theme.ts
@@ -1,10 +1,20 @@
 export default {
   palette: {
     primary: {
-      main: "hsl(213, 93%, 16%)",
+      dark: "hsl(213, 70%, 15%)",
+      main: "hsl(213, 50%, 45%)",
+      light: "hsl(213, 30%, 60%)",
+      alpha: "hsla(213, 50%, 45%, 0.2)",
     },
     secondary: {
-      main: "hsl(86, 7%, 41%)",
+      dark: "hsl(86, 8%, 21%)",
+      main: "hsl(86, 3%, 41%)",
+      light: "hsl(86, 12%, 60%)",
+      alpha: "hsla(86, 12%, 60%, 0.2)",
+    },
+    common: {
+      black: "hsl(0, 0%, 0%)",
+      white: "hsl(0, 0%, 100%)",
     },
   },
   spacing: 8,
