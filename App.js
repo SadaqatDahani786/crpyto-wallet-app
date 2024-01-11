012dc8e1804ec424898e7e5dd92a078bd31d1d18
Sadaqat Dahani <sadaqat.dahani2013@gmail.com>
2024-01-11 16:42:54 +0500

    Putting everything in App component.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar as SB,
+  ScrollView,
+} from "react-native";
+import Header from "./Components/Header";
+import theme from "./Theme/theme";
+import Controls from "./Components/Controls";
+import Footer from "./Components/Footer";
+import Tabs from "./Components/Tabs";
 
+/*
+ ** ** =======================================================================
+ ** ** ** Component [App]
+ ** ** =======================================================================
+ */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Crypto!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" backgroundColor={theme.palette.primary.dark} />
+      <ScrollView style={{ height: "auto", width: "100%" }}>
+        <Header />
+        <Controls />
+        <Tabs />
+      </ScrollView>
+      <Footer />
+    </SafeAreaView>
   );
 }
 
+/*
+ ** **
+ ** ** ** Styles
+ ** **
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: theme.palette.common.black,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: Platform.OS === "android" ? SB.currentHeight : 0,
   },
 });
